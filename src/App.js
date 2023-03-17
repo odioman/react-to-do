5ba3957d76ed2d708993dfb5fb68541e98ca4c8a
Thomas Koshy <ttkoshy@gmail.com>
2023-03-17 19:18:45 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        <input type="checkbox">
+        Milk
+        </input>
+      </p>
+      <p>
+        <input type="checkbox">
+        Eggs
+        </input>
+      </p>
+      <p>
+        <input type="checkbox">
+        Butter
+        </input>
+      </p>  
+    </div>
+  );
+}
+
+export default App;
